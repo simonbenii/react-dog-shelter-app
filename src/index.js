5d90d5fd5d3aac7fa7dd634c41f4883a16f78982
“simonbenii” <110835614+simonbenii@users.noreply.github.com>
2023-11-08 12:35:23 +0100

    done 2

--- src/index.js
+++ src/index.js
@@ -2,27 +2,33 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { HashRouter, RouterProvider, Routes, Route } from 'react-router-dom';
 import "./App.css"
 
 import Public from './components/Public';
 import Details from './components/DogDetails';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Public />,
-  },
-  {
-    path: '/dogdetails/:id',
-    element: <Details />,
-  },
-]);
+// const router = HashRouter([
+//   {
+//     path: '/',
+//     element: <Public />,
+//   },
+//   {
+//     path: '/dogdetails/:id',
+//     element: <Details />,
+//   },
+// ]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    {/*     <RouterProvider router={router} /> */}
+    <HashRouter>
+      <Routes>
+        <Route path='/' element={<Public />} />
+        <Route path='/dogdetails/:id' element={<Details />} />
+      </Routes>
+    </HashRouter>
   </React.StrictMode>
 );
 
